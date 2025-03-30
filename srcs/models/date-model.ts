9a69a2cbcafbde7add764bf1c93efb7049e94e7e
Leverson Carlos <lcjohnny@hotmail.com>
2025-03-30 19:30:42 -0300

    function to create instance more eaelly

--- srcs/models/date-model.ts
+++ srcs/models/date-model.ts
@@ -1,4 +1,11 @@
 export class DateModel {
 	public Type: string;
 	public Date: Date;
+
+	public static Create(type: string, date: Date): DateModel {
+		const dateModel = new DateModel();
+		dateModel.Type = type;
+		dateModel.Date = date;
+		return dateModel;
+	}
 }
