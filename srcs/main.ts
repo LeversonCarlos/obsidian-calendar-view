6b716f90178c00614b14662d0d7ebb05468ab5cf
Leverson Carlos <lcjohnny@hotmail.com>
2025-04-05 15:07:52 -0300

    remover codigo que nao esta sendo usado

--- srcs/main.ts
+++ srcs/main.ts
@@ -1,8 +1,8 @@
-import { App, Editor, MarkdownView, Modal, Plugin } from 'obsidian';
+import { Plugin } from 'obsidian';
 import { CalendarService } from './calendar';
 import { Injector } from './Injector';
 import { IPlugin } from './IPlugin';
-import { ItemCache, ItemService } from './Item';
+import { ItemService } from './Item';
 import { SettingsService, SettingTab } from './settings';
 import { StylesService } from './styles';
 
@@ -21,21 +21,21 @@ export default class MyPlugin extends Plugin implements IPlugin {
 
 		this.registerMarkdownCodeBlockProcessor("calendar-view", CalendarService.OnRender);
 
-		// This creates an icon in the left ribbon.
+		/*
 		const ribbonIconEl = this.addRibbonIcon('dice', 'Calendar View', (evt: MouseEvent) => {
 			Injector
 				?.getInstance(ItemCache)
 				?.Log();
 		});
-
-		// Perform additional things with the ribbon
 		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		*/
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
+		/*
 		const statusBarItemEl = this.addStatusBarItem();
 		statusBarItemEl.setText('Status Bar Text');
+		*/
 
-		// This adds a simple command that can be triggered anywhere
+		/*
 		this.addCommand({
 			id: 'open-sample-modal-simple',
 			name: 'Open sample modal (simple)',
@@ -43,7 +43,9 @@ export default class MyPlugin extends Plugin implements IPlugin {
 				new SampleModal(this.app).open();
 			}
 		});
-		// This adds an editor command that can perform some operation on the current editor instance
+		*/
+
+		/*
 		this.addCommand({
 			id: 'sample-editor-command',
 			name: 'Sample editor command',
@@ -52,57 +54,20 @@ export default class MyPlugin extends Plugin implements IPlugin {
 				editor.replaceSelection('Sample Editor Command');
 			}
 		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+		*/
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SettingTab(this.app, this));
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		// this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-		// 	console.log('click', evt);
-		// });
+		/*
+		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
+			console.log('click', evt);
+		});
+		*/
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
 		// this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-
 	}
 
 	onunload() {
 	}
 
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const { contentEl } = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const { contentEl } = this;
-		contentEl.empty();
-	}
 }
